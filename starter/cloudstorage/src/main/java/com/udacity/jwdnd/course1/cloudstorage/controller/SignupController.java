591601f0d0dfeff2f6c88ddba42e7fd41b82a580
naumtinga <naumtinga@gmail.com>
2023-09-13 10:17:45 +0200

    Signup success and unsuccess message added.

--- starter/cloudstorage/src/main/java/com/udacity/jwdnd/course1/cloudstorage/controller/SignupController.java
+++ starter/cloudstorage/src/main/java/com/udacity/jwdnd/course1/cloudstorage/controller/SignupController.java
@@ -8,6 +8,7 @@ import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.ModelAttribute;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.servlet.mvc.support.RedirectAttributes;
 
 @Controller
 @RequestMapping("/signup")
@@ -25,28 +26,25 @@ public class SignupController {
     }
 
     @PostMapping()
-    public String signupUser(@ModelAttribute User user, Model model) {
+    public String signupUser(@ModelAttribute User user, Model model, RedirectAttributes redirectAttributes) {
         String signupError = null;
 
         if (!userService.isUsernameAvailable(user.getUsername())) {
-            signupError = "The username already exists.";
+            signupError = "The username is not available.";
         }
 
         if (signupError == null) {
             int rowsAdded = userService.createUser(user);
             if (rowsAdded < 0) {
                 signupError = "There was an error signing you up. Please try again.";
-                model.addAttribute("result", "notSaved");
+            } else {
+                redirectAttributes.addFlashAttribute("signupSuccess", true);
+                return "redirect:/login"; // Redirect to the login page with the success message.
             }
-        }
-
-        if (signupError == null) {
-            model.addAttribute("signupSuccess", true);
-            model.addAttribute("result", "success");
         } else {
             model.addAttribute("signupError", signupError);
         }
 
-        return "login";
+        return "signup"; // Return to the signup page with an error message if signup fails.
     }
 }
